5384ae80ed35e4638697e9a1b177a8889884508e
VIGNESHTHANGAVEL1 <vigneshtskv96@gmail.com>
2025-09-28 11:31:14 +0530

    Remove backend files and update frontend

--- backend/routes/questions.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getQuestionsByTopic } = require('../controllers/questionsController');
-
-router.get('/:topic', getQuestionsByTopic);
-
-module.exports = router;
